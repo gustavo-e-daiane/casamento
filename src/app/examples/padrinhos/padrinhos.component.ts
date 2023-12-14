5c275fd002d3b9faa33a76246081fd207dc72b83
gustavo.mmello <gustavo.mello@farmaciasassociadas.com.br>
2023-12-14 13:37:43 -0300

    adicionando as imagens dos padrinhos

--- src/app/examples/padrinhos/padrinhos.component.ts
+++ src/app/examples/padrinhos/padrinhos.component.ts
@@ -2,9 +2,10 @@ import { Component, OnInit } from "@angular/core";
 import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 import { NgbdModalContent } from "app/components/modal/modal.component";
 interface padrinho {
-  nome: string;
+  nome: string,
+  image: string,
   categoria: string,
-  description: string;
+  description: string
 }
 @Component({
   selector: "app-padrinhos",
@@ -16,6 +17,7 @@ export class PadrinhosComponent implements OnInit {
   padrinhos: padrinho[] = [
     {
       nome: "Antony e Patrícia",
+      image: "./assets/img/padrinhos/bolzan-mota.jpg",
       categoria: "Colegas da noiva",
       description: `Antoni e Patrícia Antoni,
       é o melhor amigo da noiva, se conheceram durante uma
@@ -28,54 +30,57 @@ export class PadrinhosComponent implements OnInit {
     },
     {
       nome: "Emilly Vitória",
+      image: "./assets/img/padrinhos/vitoria.jpg",
       categoria: 'Irmã da noiva',
       description: `Ela é amiga e parceira incondicional da noiva, juntas compartilharam momentos icônicos que ficaram gravados em suas memórias para sempre. É com quem ela sempre pode contar independente da distância e do tamanho do desafio a enfrentar .
 Acompanhou o relacionamento desde os primeiros passos e sempre ofereceu todo apoio e carinho ao casal.
 No dia do casamento, ela é essencial ao lado da noiva pois desde o início é a testemunha de todo amor que o casal compartilha.` },
     {
       nome: "Adrian",
-      categoria: ' melhor amiga da noiva',
-      description: `Uma amizade que começou na escola e já dura mais de 15 anos, Adrian é mais do que uma confidente, ela é aquela pessoa que está sempre ao seu lado, ouvindo, apoiando e compartilhando momentos inesquecíveis.
-      Nesse dia tão importante, ela é a pessoa perfeita para estar ao lado da noiva, celebrando o amor e a amizade.`
-    },
-    {
-      nome: "Adrian",
+      image: "./assets/img/padrinhos/adrian.jpg",
       categoria: ' melhor amiga da noiva',
       description: `Uma amizade que começou na escola e já dura mais de 15 anos, Adrian é mais do que uma confidente, ela é aquela pessoa que está sempre ao seu lado, ouvindo, apoiando e compartilhando momentos inesquecíveis.
       Nesse dia tão importante, ela é a pessoa perfeita para estar ao lado da noiva, celebrando o amor e a amizade.`
     },
     {
       nome: "Rafa",
+      image: "./assets/img/padrinhos/a.jpg",
       categoria: 'Irmáo gêmeo do noivo',
       description: `Irmão`
     },
     {
       nome: "Deise e Reginaldo",
+      image: "./assets/img/padrinhos/regi-deise.jpg",
       categoria: 'Irmã da noiva e marído',
       description: `casal`
     },
     {
       nome: "Franco e Jessica",
+      image: "./assets/img/padrinhos/franco-jessica.jpg",
       categoria: 'Casal',
       description: `casal`
     },
     {
       nome: "Alam e Ticy",
+      image: "./assets/img/padrinhos/alam-ticy.jpg",
       categoria: 'Casal',
       description: `casal`
     },
     {
       nome: "Diogo",
+      image: "./assets/img/padrinhos/a.jpg",
       categoria: 'vesgo',
       description: `vesgo`
     },
     {
       nome: "Said",
+      image: "./assets/img/padrinhos/a.jpg",
       categoria: 'turco',
       description: `turco`
     },
     {
       nome: "Elis",
+      image: "./assets/img/padrinhos/b.jpg",
       categoria: 'colega da noiva',
       description: `colega`
     },
